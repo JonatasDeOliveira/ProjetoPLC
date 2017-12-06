284308468c2c00ff8df7de6d1a3de1dc8aacb893
rlo <rlo@cin.ufpe.br>
2017-12-06 18:51:51 -0300

    Small chnages in Menu

--- src/ourGame/Menu.java
+++ src/ourGame/Menu.java
@@ -33,7 +33,7 @@ public class Menu extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Menu frame = new Menu();
+					Menu frame = new Menu(player_images);
 					frame.setVisible(true);
 
 				} catch (Exception e) {
@@ -44,7 +44,7 @@ public class Menu extends JFrame {
 	}
 	private int count;
 	private JButton btn_StarGame;
-	private ImageIcon[] player_images = new ImageIcon[3];
+	private static ImageIcon[] player_images = new ImageIcon[3];
 
 
 	private JLabel lblSelectYourPlayer;
@@ -120,7 +120,7 @@ public class Menu extends JFrame {
 		lblSelectYourPlayer.setHorizontalAlignment(SwingConstants.CENTER);
 		lblSelectYourPlayer.setForeground(new Color(255, 140, 0));
 		lblSelectYourPlayer.setFont(new Font("Courier New", Font.PLAIN, 48));
-		lblSelectYourPlayer.setBounds(109, 167, 846, 111);
+		lblSelectYourPlayer.setBounds(124, 167, 846, 111);
 		getContentPane().add(lblSelectYourPlayer);
 
 		t = new javax.swing.Timer(300, new ActionListener() {
@@ -172,7 +172,7 @@ public class Menu extends JFrame {
 		name_game.setHorizontalAlignment(SwingConstants.CENTER);
 		name_game.setForeground(new Color(255, 140, 0));
 		name_game.setFont(new Font("Courier New", Font.PLAIN, 73));
-		name_game.setBounds(315, 57, 435, 111);
+		name_game.setBounds(277, 60, 540, 111);
 		getContentPane().add(name_game);
 
 		JLabel fundo_menu = new JLabel("");
